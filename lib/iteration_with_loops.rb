e2e61521c43dbe5fb797ebc582e108e17aa0c4a6
Troy Domio <troyakins99@yahoo.com>
2020-05-16 05:30:57 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,7 +1,7 @@
 def join_nested_strings (src)
  row_index = 0 
   newnew = []
- src.flatten!
+ 
   while row_index < src.count do
   element_index = 0 
  while element_index < src[row_index].count do 
